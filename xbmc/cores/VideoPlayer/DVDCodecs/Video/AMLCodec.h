5b1ceb27e036e08bce3b158231cfd5fb10d13d01
Portisch <hugo.portisch@yahoo.de>
2023-02-28 09:48:47 +0100

    AMLCodec: rework and update 'GetPicture' and 'AddData' Remove not needed delays. Check correct free_len when adding new chunk to hardware decoder input buffer. 'DequeueBuffer': do return EAGAIN when no new frame is decoded. But this does not mean the frame buffer is empty at all. Reset the decoder when no new frame is available within the new timeout, default is 5s. 'GetPicture': signal VC_NONE if DequeueBuffer failed and hardware buffer is not empty

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/AMLCodec.h
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/AMLCodec.h
@@ -94,8 +94,9 @@ private:
   void          CloseAmlVideo();
   std::string   GetVfmMap(const std::string &name);
   void          SetVfmMap(const std::string &name, const std::string &map);
+  float         GetBufferLevel();
+  float         GetBufferLevel(int new_chunk, int &data_len, int &free_len);
   int           DequeueBuffer();
-  float         GetTimeSize();
   unsigned int  GetDecoderVideoRate();
   std::string   GetHDRStaticMetadata();
 
@@ -129,10 +130,9 @@ private:
   PosixFilePtr     m_amlVideoFile;
   std::string      m_defaultVfmMap;
 
-  std::deque<uint32_t> m_frameSizes;
-  std::uint32_t m_frameSizeSum;
-
   static std::atomic_flag  m_pollSync;
   static int m_pollDevice;
   CProcessInfo &m_processInfo;
+  int m_decoder_timeout;
+  std::chrono::time_point<std::chrono::system_clock> m_tp_last_frame;
 };
