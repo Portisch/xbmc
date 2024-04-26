71957d819dc2d7e5e42da7032cbe5faf91c66f4e
Portisch <hugo.portisch@yahoo.de>
2024-04-26 11:45:53 +0200

    BitstreamConverter: add new overload Convert to combine base and enhancement layer packages Convert packages to bitstream and combine both packages to a single bytestream package.

--- xbmc/utils/BitstreamConverter.h
+++ xbmc/utils/BitstreamConverter.h
@@ -111,6 +111,7 @@ public:
   void              Close(void);
   bool NeedConvert(void) const { return m_convert_bitstream; }
   bool              Convert(uint8_t *pData, int iSize);
+  bool              Convert(uint8_t *pData_bl, int iSize_bl, uint8_t *pData_el, int iSize_el);
   uint8_t*          GetConvertBuffer(void) const;
   int               GetConvertSize() const;
   uint8_t* GetExtraData();
@@ -144,6 +145,11 @@ protected:
                                     const uint8_t* in,
                                     uint32_t in_size,
                                     uint8_t nal_type);
+  static void BitstreamAllocAndCopy(uint8_t** poutbuf,
+                                    uint32_t* poutbuf_size,
+                                    const uint8_t* in,
+                                    uint32_t in_size,
+                                    uint8_t nal_type);
 
 #ifdef HAVE_LIBDOVI
   const DoviData* processDoviRpu(uint8_t* buf, uint32_t nalSize);
@@ -166,6 +172,7 @@ protected:
   omx_bitstream_ctx m_sps_pps_context;
   bool              m_convert_bitstream;
   bool              m_to_annexb;
+  bool              m_combine;
 
   FFmpegExtraData m_extraData;
   bool              m_convert_3byteTo4byteNALSize;
