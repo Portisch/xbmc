22cde3282b986af8b43f40aee0a2bfefc1cc5357
Portisch <hugo.portisch@yahoo.de>
2025-02-14 10:37:40 +0100

    Drop GUI scaling GUI scaling is not need anymore because of commit https://github.com/CoreELEC/common_drivers/commit/f65a08e06a6eabe90a12261d14c38e0ddfb4abc1

--- xbmc/settings/Settings.h
+++ xbmc/settings/Settings.h
@@ -466,7 +466,6 @@ public:
   static constexpr auto SETTING_COREELEC_AMLOGIC_HDR2SDR = "coreelec.amlogic.hdr2sdr";
   static constexpr auto SETTING_COREELEC_AMLOGIC_LIMIT_CD = "coreelec.amlogic.limitcd";
   static constexpr auto SETTING_COREELEC_AMLOGIC_FORCE_CS = "coreelec.amlogic.forcecs";
-  static constexpr auto SETTING_COREELEC_AMLOGIC_DISABLEGUISCALING = "coreelec.amlogic.disableguiscaling";
   static constexpr auto SETTING_COREELEC_AMLOGIC_DV_DISABLE = "coreelec.amlogic.disabledolbyvision";
   static constexpr auto SETTING_COREELEC_AMLOGIC_DV_LED = "coreelec.amlogic.dolbyvisionled";
   static constexpr auto SETTING_CACHE_HARDDISK = "cache.harddisk";
