70a6f3e26fd94c65297af22e727d31afec13e206
quietvoid <tcChlisop0@gmail.com>
2025-11-04 06:35:26 -0500

    Add Dolby Vision setting to set zero L5 metadata

--- xbmc/cores/VideoPlayer/MediaPipelineWebOS.cpp
+++ xbmc/cores/VideoPlayer/MediaPipelineWebOS.cpp
@@ -854,6 +854,7 @@ void CMediaPipelineWebOS::SetupBitstreamConverter(CDVDStreamInfo& hint)
   const std::shared_ptr<CSettings> settings = CServiceBroker::GetSettingsComponent()->GetSettings();
   const bool convertDovi =
       hint.dovi.el_present_flag || settings->GetBool(CSettings::SETTING_VIDEOPLAYER_CONVERTDOVI);
+  const bool doviZeroLevel5 = settings->GetBool(CSettings::SETTING_VIDEOPLAYER_DOVIZEROLEVEL5);
 
   const std::shared_ptr allowedHdrFormatsSetting(std::dynamic_pointer_cast<CSettingList>(
       settings->GetSetting(CSettings::SETTING_VIDEOPLAYER_ALLOWEDHDRFORMATS)));
@@ -872,6 +873,7 @@ void CMediaPipelineWebOS::SetupBitstreamConverter(CDVDStreamInfo& hint)
         if (hint.codec == AV_CODEC_ID_HEVC)
         {
           m_bitstream->SetRemoveDovi(removeDovi);
+          m_bitstream->SetDoviZeroLevel5(doviZeroLevel5);
 
           // webOS doesn't support HDR10+ and it can cause issues
           m_bitstream->SetRemoveHdr10Plus(true);
